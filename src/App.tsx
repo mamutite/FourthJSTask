85d328c3005e812c3ab6af7ee12fdfd3f218e9e2
Stefan Galabov <sgalabov22@gmail.com>
2022-08-25 17:47:09 +0300

    Moved API keys to env file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import Autocomplete from "react-google-autocomplete";
 
-const YOUR_GOOGLE_MAPS_API_KEY = "AIzaSyBlzs88A9ls5GLMa0LW5Sl8ge8uzVcPTFY";
-const WEATHER_API_KEY = "8929a8f81f83e6cb778920ccf1b18e1d";
-
 interface AddressComponentI {
   long_name: string;
   short_name: string;
@@ -23,7 +20,7 @@ function App() {
           const lon = position.coords.longitude;
 
           fetch(
-            `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+            `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
           )
             .then((res) => res.json())
             .then((result) => {
@@ -38,11 +35,11 @@ function App() {
   }
 
   function handlePlaceSelected(place: PlaceResultI) {
-    console.log(place)
+    console.log(place);
     if (place === undefined) return;
     const city = place.address_components[0].long_name;
     fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${WEATHER_API_KEY}&units=metric`
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
     )
       .then((res) => res.json())
       .then((result) => {
@@ -53,11 +50,9 @@ function App() {
   return (
     <div className="App">
       <Autocomplete
-        apiKey={YOUR_GOOGLE_MAPS_API_KEY}
+        apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
         lang="en"
-        onPlaceSelected={(place: any) =>
-          handlePlaceSelected(place)
-        }
+        onPlaceSelected={(place: any) => handlePlaceSelected(place)}
       />
       <div className="current-location">
         <button onClick={getLocation}>Get location</button>
